e259e13db5a9a8cda4556cfe3509ac12e9fd7e8c
CodySawa <kota418@gmail.com>
2022-10-22 13:23:10 -0700

    add error handling for sign up

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,8 @@
+import imp
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api') 
 
@@ -9,14 +11,27 @@ def signup():
     data = request.get_json()
     db = get_db()
     
-    newUser = User(
-        username = data['username'],
-        email = data['email'],       
-        password = data['password']
-    )
+    try:
+        # attempt creating a new user
+        newUser = User(
+            username = data['username'],
+            email = data['email'],       
+            password = data['password']
+        )
 
-    # save in database
-    db.add(newUser)
-    db.commit()
+        # save in database
+        db.add(newUser)
+        db.commit()
+        print('success')
+        
+    except AssertionError:
+        print('validation error')
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
+
+    except sqlalchemy.exc.IntegrityError:
+        print('mysql error')
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
 
     return jsonify(id = newUser.id)
