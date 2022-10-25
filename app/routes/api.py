ee944cf31c45ba2a0620e3461041af5e2509ef67
CodySawa <kota418@gmail.com>
2022-10-25 16:48:17 -0700

    create API to upvote and comment on posts

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,23 +1,25 @@
+from crypt import methods
 from email import message
 import imp
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
 
-bp = Blueprint('api', __name__, url_prefix='/api') 
+bp = Blueprint('api', __name__, url_prefix='/api')
+
 
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
     db = get_db()
-    
+
     try:
         # attempt creating a new user
         newUser = User(
-            username = data['username'],
-            email = data['email'],       
-            password = data['password']
+            username=data['username'],
+            email=data['email'],
+            password=data['password']
         )
 
         # save in database
@@ -28,23 +30,25 @@ def signup():
     except AssertionError:
         print('validation error')
         db.rollback()
-        return jsonify(message = 'Signup failed'), 500
+        return jsonify(message='Signup failed'), 500
 
     except sqlalchemy.exc.IntegrityError:
         print('mysql error')
         db.rollback()
-        return jsonify(message = 'Signup failed'), 500
+        return jsonify(message='Signup failed'), 500
 
     session.clear()
     session['user_id'] = newUser.id
     session['loggedIn'] = True
-    return jsonify(id = newUser.id)
+    return jsonify(id=newUser.id)
+
 
 @bp.route('/users/logout', methods=['POST'])
 def logout():
     # remove session variables
     session.clear()
     return '', 204
+
 
 @bp.route('/users/login', methods=['POST'])
 def login():
@@ -56,12 +60,59 @@ def login():
     except:
         print(sys.exc_info()[0])
 
-        return jsonify(message = 'Incorrect credentials'), 400
+        return jsonify(message='Incorrect credentials'), 400
 
     if user.verify_password(data['password']) == False:
-        return jsonify(message = 'Incorrect credentials'), 400
+        return jsonify(message='Incorrect credentials'), 400
 
     session.clear()
     session['user_id'] = user.id
     session['loggedIn'] = True
-    return jsonify(id = user.id)    
+    return jsonify(id=user.id)
+
+
+@bp.route('/comments', methods=['POST'])
+def comment():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # create a new comment
+        newComment = Comment(
+            comment_text=data['comment_text'],
+            post_id=data['post_id'],
+            user_id=session.get('user_id')
+        )
+
+        db.add(newComment)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message='Comment failed'), 500
+
+    return jsonify(id=newComment.id)
+
+
+@bp.route('/posts/upvote', methods=['PUT'])
+def upvote():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # create a new vote with incoming id and session id
+        newVote = Vote(
+            post_id=data['post_id'],
+            user_id=session.get('user_id')
+        )
+
+        db.add(newVote)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message='Upvote failed'), 500
+
+    return '', 204
